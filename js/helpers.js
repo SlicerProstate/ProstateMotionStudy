1332277c67a9fe3893ee0363c0b8e44f535995a2
Christian Herz <christian.herz@mevis.fraunhofer.de>
2015-10-02 15:13:44 -0400

    ENH: extended website by assessement possibility - TODO: sending JSON to google spreadsheet

--- js/helpers.js
+++ js/helpers.js
@@ -4,11 +4,88 @@
  * and open the template in the editor.
  */
 
-var patientId = 11;
 var db = null;
+var expanded = false;
+var rating = null;
 
-setupDatabaseConnection();
+$(document).ready(function() {
 
+
+    $('input[name="rating"]').change(function(){
+        $("#ratingResultDisplay").text($(this).next('label').attr("title"));
+        rating = $(this).val()
+    });
+
+    $("#HematomaCheckbox").change(function() {
+        if (!this.checked) {
+            closeLocationDropdown();
+        } else {
+            clearLocationCheckboxes();
+        }
+    });
+
+    $("#submitForm").submit(function() {
+        var locations = [];
+        var hematoma = $("#HematomaCheckbox")[0].checked;
+        if (hematoma)
+            locations = getCheckedLocations();
+
+        // check for valid data
+        // create json data from form
+        if (rating == null) {
+            alert("Please rate the registration result!")
+        } else {
+            $.post($(this).attr('action'), $(this).serialize(), function (response) {
+                // do something here on success
+            }, 'json');
+            rating = null;
+            document.getElementById("submitForm").reset();
+            closeLocationDropdown();
+        }
+        return false;
+    });
+    setupDatabaseConnection();
+});
+
+function closeLocationDropdown() {
+    expanded = false;
+    showCheckboxes();
+}
+
+
+function getCheckedLocations() {
+    var checkboxes = $("#checkboxes");
+    var locations = checkboxes.find("input");
+    var checkedLocations = [];
+    locations.each(function(index, checkbox) {
+        if(checkbox.checked) {
+            checkedLocations.push(checkbox.value);
+        }
+    });
+    return checkedLocations
+}
+
+function clearLocationCheckboxes() {
+    var checkboxes = $("#checkboxes");
+    var locations = checkboxes.find("input");
+    locations.each(function(index, checkbox) {
+        checkbox.checked = false;
+    });
+}
+
+function showCheckboxes() {
+    var checkboxes = document.getElementById("checkboxes");
+    var hematoma = document.getElementById("HematomaCheckbox").checked;
+    var width = $("#locations").width();
+    if (!expanded && hematoma) {
+        checkboxes.style.display = "block";
+        checkboxes.style.width = width-2+"px";
+        expanded = true;
+    } else {
+        checkboxes.style.display = "none";
+        expanded = false;
+    }
+}
 
 function setupDatabaseConnection() {
     var xhr = new XMLHttpRequest();
@@ -54,7 +131,7 @@ function updateDSCForPatientId(patientId) {
 function updateView() {
     var dropdown = $('#needleDropDown')[0];
     var option = dropdown.options[dropdown.selectedIndex];
-    setNeedleImage(option.value)
+    setNeedleImage(option.value);
     $("#imageToSwap").css("visibility", "visible");
 }
 
@@ -100,4 +177,7 @@ function setNeedleImage(imageId) {
     setRegistrationMode(false);
 }
 
+
+// 1. when other resluts are displayed, check if assessment if evaluation has already be done and disable if so
+// 2. checking hematoma activates widget for checking locations
 
